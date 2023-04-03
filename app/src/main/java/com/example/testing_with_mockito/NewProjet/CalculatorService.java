354e745cecce840d466cdc92df2766e6a838c5b9
Mohamed <mohamedelghamraoui@gmail.com>
2023-04-03 16:36:19 +0000

    Testing_With_Mokhito

---
+++ app/src/main/java/com/example/testing_with_mockito/NewProjet/CalculatorService.java
@@ -0,0 +1,20 @@
+package com.example.testing_with_mockito.NewProjet;
+
+public class CalculatorService {
+    public double add(double input1, double input2) {
+        return 0;
+    }
+
+    public double sous(double input1, double input2) {
+        return 0;
+    }
+
+    public double multi(double input1, double input2) {
+        return 0;
+    }
+
+    public double div(double input1, double input2) {
+        return 0;
+    }
+
+}
